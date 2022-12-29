4926183287927131b019d6fdfd31fe0cea3b053f
Jonathan Sutton <jonny.a.sutton@gmail.com>
2022-12-29 17:54:38 +0100

    read excel, download images

---
+++ script.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# Read the spreadsheet into a dataframe
+df = pd.read_excel('data/librarything_kokestu.xlsx')
+
+# Print the first few rows of the dataframe
+print(df.head())
+
+#################
+# Read the HTML #
+#################
+from bs4 import BeautifulSoup
+
+with open('data/book-covers.htm', 'r') as f:
+    html_content = f.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find img elements -- these are ordered by ISBN
+imgs = soup.find_all('img')
+urls = [img.get("src") for img in imgs]  # gets the image urls
+
+#######################
+# Download the images #
+#######################
+import requests
+
+for url in urls:
+    # Download the image
+    response = requests.get(url)
+    image_data = response.content
+
+    # Save the image to a file
+    file_name = url.split('/')[-1]
+    with open(f"data/imgs/{file_name}", 'wb') as f:
+        f.write(image_data)
